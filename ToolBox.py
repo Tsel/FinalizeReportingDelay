7b93db814e58ea36b013357f6ac0a266c242d934
Tsel <thomas.selhorst@online.de>
2018-02-17 17:13:40 +0100

    Started Reporting Delay file Started ToolBox File

---
+++ ToolBox.py
@@ -0,0 +1,15 @@
+"""
+This file contains special tools for network analysis
+created Feb 16, 2018
+"""
+
+def accessibilitymatrix(A_t):
+    """
+
+    :param A_t: 3d matrix n x n x t, with temporal (t) shnapshots of the network (adjacency matrix A n x n)
+    consisting of n vertices
+    """
+
+
+if __name__ == "__main__":
+    pass
